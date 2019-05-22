b89252ab300294fc8aff25bd4ccb31588cd6b595
e7e7 <50891896+e7e7@users.noreply.github.com>
2019-05-22 02:51:12 +0200

    alias functions

--- lib_materials_utils.lua
+++ lib_materials_utils.lua
@@ -1,4 +1,5 @@
 
+
 function table.contains_substring(t, s)
 	if type(s) ~= "string" then
 		return nil
@@ -34,3 +35,32 @@ function get_node_drops(fullRockNode, cobbleRockNode)
 	}
 end
 
+lib_materials.register_craftitem = function(name, alt, def)
+	if minetest.registered_craftitems[alt] then
+		minetest.override_item(alt, def)
+		minetest.register_alias(name, alt)
+	else
+		minetest.register_craftitem(name, def)
+		minetest.register_alias(alt, name)
+	end
+end
+
+lib_materials.register_node = function(name, alt, def)
+	if minetest.registered_nodes[alt] then
+		minetest.override_item(alt, def)
+		minetest.register_alias(name, alt)
+	else
+		minetest.register_node(name, def)
+		minetest.register_alias(alt, name)
+	end
+end
+
+lib_materials.register_tool = function(name, alt, def)
+	if minetest.registered_tools[alt] then
+		minetest.override_item(alt, def)
+		minetest.register_alias(name, alt)
+	else
+		minetest.register_tool(name, def)
+		minetest.register_alias(alt, name)
+	end
+end
