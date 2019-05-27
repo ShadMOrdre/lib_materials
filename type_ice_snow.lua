3485c99f070dddec7c208ab59b17bbf2b73f7799
e7e7 <50891896+e7e7@users.noreply.github.com>
2019-05-27 15:09:39 +0200

    alias

--- type_ice_snow.lua
+++ type_ice_snow.lua
@@ -1,24 +1,22 @@
-
-
-
 local S = lib_materials.intllib
 
-
-
 -- Snow
-minetest.register_node("lib_materials:snow_brick", {
-	description = S("Snow Brick"),
-	tiles = {"lib_materials_snow_brick.png"},
-	paramtype = "light",
-	freezemelt = "default:water_source",
-	is_ground_content = false,
-	groups = {crumbly = 3, puts_out_fire = 1, cools_lava = 1},
-	sounds = default.node_sound_dirt_defaults({
-		footstep = {name = "default_snow_footstep", gain = 0.15},
-		dug = {name = "default_snow_footstep", gain = 0.2},
-		dig = {name = "default_snow_footstep", gain = 0.2},
-	}),
-})
+lib_materials.register_node(
+	"lib_materials:snow_brick", "ethereal:snowbrick", 
+	{
+		description = S("Snow Brick"),
+		tiles = {"lib_materials_snow_brick.png"},
+		paramtype = "light",
+		freezemelt = "default:water_source",
+		is_ground_content = false,
+		groups = {crumbly = 3, puts_out_fire = 1, cools_lava = 1},
+		sounds = default.node_sound_dirt_defaults({
+			footstep = {name = "default_snow_footstep", gain = 0.15},
+			dug = {name = "default_snow_footstep", gain = 0.2},
+			dig = {name = "default_snow_footstep", gain = 0.2},
+		}),
+	}
+)
 
 minetest.register_craft({
 	output = 'lib_materials:snow_brick 4',
@@ -28,7 +26,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_node("lib_materials:snow", {
+lib_materials.register_node("lib_materials:snow", "default:snow", {
 	description = "Snow",
 	tiles = {"lib_materials_snow.png"},
 	inventory_image = "lib_materials_snowball.png",
@@ -54,50 +52,60 @@ minetest.register_node("lib_materials:snow", {
 
 	on_construct = function(pos)
 		pos.y = pos.y - 1
+		-- FIXME: this check needs to be adjusted
 		if minetest.get_node(pos).name == "default:dirt_with_grass" then
 			minetest.set_node(pos, {name = "default:dirt_with_snow"})
 		end
 	end,
 })
 
-minetest.register_node("lib_materials:snow_block", {
-	description = "Snow Block",
-	tiles = {"lib_materials_snow.png"},
-	groups = {crumbly = 3, puts_out_fire = 1, cools_lava = 1, snowy = 1},
-	sounds = default.node_sound_dirt_defaults({
-		footstep = {name = "default_snow_footstep", gain = 0.15},
-		dug = {name = "default_snow_footstep", gain = 0.2},
-		dig = {name = "default_snow_footstep", gain = 0.2}
-	}),
+lib_materials.register_node(
+	"lib_materials:snow_block", "default:snowblock", {
+		description = "Snow Block",
+		tiles = {"lib_materials_snow.png"},
+		groups = {
+			crumbly = 3, puts_out_fire = 1, cools_lava = 1, snowy = 1
+		},
+		sounds = default.node_sound_dirt_defaults({
+			footstep = {name = "default_snow_footstep", gain = 0.15},
+			dug = {name = "default_snow_footstep", gain = 0.2},
+			dig = {name = "default_snow_footstep", gain = 0.2}
+		}),
 
-	on_construct = function(pos)
-		pos.y = pos.y - 1
-		if minetest.get_node(pos).name == "default:dirt_with_grass" then
-			minetest.set_node(pos, {name = "default:dirt_with_snow"})
-		end
-	end,
-})
+		on_construct = function(pos)
+			pos.y = pos.y - 1
+			-- FIXME: this check needs to be adjusted
+			if minetest.get_node(pos).name == "default:dirt_with_grass" then
+				minetest.set_node(pos, {name = "default:dirt_with_snow"})
+			end
+		end,
+	}
+)
 
 -- Ice
-minetest.register_node("lib_materials:ice_default", {
-	description = "Ice - Default",
-	tiles = {"lib_materials_ice.png"},
-	is_ground_content = false,
-	paramtype = "light",
-	groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
-	sounds = default.node_sound_glass_defaults(),
-})
+lib_materials.register_node(
+	"lib_materials:ice_default", "default:ice", {
+		description = "Ice - Default",
+		tiles = {"lib_materials_ice.png"},
+		is_ground_content = false,
+		paramtype = "light",
+		groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
+		sounds = default.node_sound_glass_defaults(),
+	}
+)
 
-minetest.register_node("lib_materials:ice_default2", {
-	description = "Ice - Default 2",
-	tiles = {"lib_materials_ice2.png"},
-	is_ground_content = false,
-	paramtype = "light",
-	groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
-	sounds = default.node_sound_glass_defaults(),
-})
+lib_materials.register_node(
+	"lib_materials:ice_default2", "default:cave_ice", {
+		description = "Ice - Default 2",
+		tiles = {"lib_materials_ice2.png"},
+		is_ground_content = false,
+		paramtype = "light",
+		groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
+		sounds = default.node_sound_glass_defaults(),
+	}
+)
 
-minetest.register_node("lib_materials:ice_block", {
+lib_materials.register_node("lib_materials:ice_block", nil, {
 	description = "Ice - Block",
 	tiles = {"lib_materials_ice_block.png"},
 	is_ground_content = false,
@@ -106,15 +114,17 @@ minetest.register_node("lib_materials:ice_block", {
 	sounds = default.node_sound_glass_defaults(),
 })
 
-minetest.register_node("lib_materials:ice_brick", {
-	description = S("Ice Brick"),
-	tiles = {"lib_materials_ice_brick.png"},
-	paramtype = "light",
-	freezemelt = "default:water_source",
-	is_ground_content = false,
-	groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
-	sounds = default.node_sound_glass_defaults(),
-})
+lib_materials.register_node(
+	"lib_materials:ice_brick", "ethereal:icebrick", {
+		description = S("Ice Brick"),
+		tiles = {"lib_materials_ice_brick.png"},
+		paramtype = "light",
+		freezemelt = "default:water_source",
+		is_ground_content = false,
+		groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
+		sounds = default.node_sound_glass_defaults(),
+	}
+)
 
 minetest.register_craft({
 	output = 'lib_materials:ice_brick 4',
@@ -124,7 +134,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_node("lib_materials:ice_thin", {
+lib_materials.register_node("lib_materials:ice_thin", nil, {
 	description = "Ice - Thin",
 	tiles = {"lib_materials_ice_thin.png"},
 	is_ground_content = false,
