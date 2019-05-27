2522719394587d1a14135217eb1346a03a9492a2
e7e7 <50891896+e7e7@users.noreply.github.com>
2019-05-27 15:12:11 +0200

    alias

--- type_sand.lua
+++ type_sand.lua
@@ -1,52 +1,69 @@
-
 local S = lib_materials.intllib
 
+	lib_materials.register_node(
+		"lib_materials:sand", "default:sand", {
+			description = "Sand - Sand Default",
+			tiles = {"lib_materials_sand_sand_default.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		}
+	)
+	lib_materials.register_node(
+		"lib_materials:sand_beach", nil, {
+			description = "Sand - Beach",
+			tiles = {"lib_materials_sand_sand_default.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		}
+	)
+	lib_materials.register_node(
+		"lib_materials:sand_desert", "default:desert_sand", {
+			description = "Sand - Desert Default",
+			tiles = {"lib_materials_sand_desert_default.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		}
+	)
+	lib_materials.register_node(
+		"lib_materials:sand_silver", "default:silver_sand", {
+			description = "Sand - Silver Default",
+			tiles = {"lib_materials_sand_silver_default.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		}
+	)
+	lib_materials.register_node(
+		"lib_materials:sand_volcanic", nil, {
+			description = ("Sand - Volcanic"),
+			tiles = {"lib_materials_sand_volcanic.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		}
+	)
+	lib_materials.register_node(
+		"lib_materials:sand_white", nil, {
+			description = ("Sand - White"),
+			tiles = {"lib_materials_sand_white.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		}
+	)
 
-	minetest.register_node("lib_materials:sand", {
-		description = "Sand - Sand Default",
-		tiles = {"lib_materials_sand_sand_default.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		sounds = default.node_sound_sand_defaults(),
-	})
-	minetest.register_node("lib_materials:sand_beach", {
-		description = "Sand - Beach",
-		tiles = {"lib_materials_sand_sand_default.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		sounds = default.node_sound_sand_defaults(),
-	})
-	minetest.register_node("lib_materials:sand_desert", {
-		description = "Sand - Desert Default",
-		tiles = {"lib_materials_sand_desert_default.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		sounds = default.node_sound_sand_defaults(),
-	})
-	minetest.register_node("lib_materials:sand_silver", {
-		description = "Sand - Silver Default",
-		tiles = {"lib_materials_sand_silver_default.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		sounds = default.node_sound_sand_defaults(),
-	})
-	minetest.register_node("lib_materials:sand_volcanic", {
-		description = ("Sand - Volcanic"),
-		tiles = {"lib_materials_sand_volcanic.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		sounds = default.node_sound_sand_defaults(),
-	})
-	minetest.register_node("lib_materials:sand_white", {
-		description = ("Sand - White"),
-		tiles = {"lib_materials_sand_white.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		sounds = default.node_sound_sand_defaults(),
-	})
-
-	minetest.register_node("lib_materials:sand_with_rocks", {
-		description = "Sand and rocks",
-		tiles = {"lib_materials_sand_with_rocks.png"},
-		groups = {crumbly = 3, falling_node = 1, sand = 1},
-		--sounds = default.node_sound_sand_defaults(),
-		drop = {max_items=2, items={{items={"lib_materials:small_rocks"}, rarity=1}}},
-	})
-
+	lib_materials.register_node(
+		"lib_materials:sand_with_rocks", nil, {
+			description = "Sand and rocks",
+			tiles = {"lib_materials_sand_with_rocks.png"},
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			--sounds = default.node_sound_sand_defaults(),
+			drop = {
+				max_items=2,
+				items={{
+					items={"lib_materials:small_rocks"},
+					rarity=1
+				}}
+			}
+		}
+	)
 
 -- Prehistoric Life Sand
 -- Node Texture Modifier
@@ -60,10 +77,3 @@ local S = lib_materials.intllib
 -- newnode.drop = {max_items=2, items={{items={"lib_materials:small_rocks"}, rarity=1}, {items={"default:sand"}, rarity=1}}}
 -- minetest.register_node("lib_materials:sand_with_rocks", newnode)
 -- end
-
-
-
-
-
-
-
